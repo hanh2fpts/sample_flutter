cb961ad8e4c79ef4a598d5837706a9483dc64fe4
Ha Nguyen Hung <hanh2@fpts.com.vn>
2022-11-02 08:56:53 +0700

    test commit win

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:sample_flutter/app/app.dart';
 
 import 'package:sample_flutter/main.dart';
 
